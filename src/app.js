43cfc887e01875724ab15dde23e519ea987465f1
Hanlun Wang <54944918+HanlunWang@users.noreply.github.com>
2023-07-19 14:27:48 -0400

    Create code-table-react

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<p>This is a Boilerplate</p>);
